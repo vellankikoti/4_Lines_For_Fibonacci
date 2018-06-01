aa7633dafcb5bf65bf22968ff6e22910e481160a
Koti Vellanki <vellankikoti@gmail.com>
2018-06-01 07:41:40 +0530

    Add files via upload

---
+++ 4_lines_fibonacci.py
@@ -0,0 +1,10 @@
+#Fibonacci Sequence in $ Lines
+
+# Defining variables and assigning values to them
+a,b = 0,1
+# Specifying limit for Fibonacci Sequence
+while(b<50):
+	#Swapping the values
+	a,b = b,a+b
+	#print the values
+	print(a)	
